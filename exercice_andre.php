36582d35d6c1c0f143f522804cfb2a09fcb28362
QuentinFayt <quentin.fayt.p@gmail.com>
2021-07-05 13:40:32 +0200

    correction

--- exercice_andre.php
+++ exercice_andre.php
@@ -4,10 +4,10 @@ for ($n=1; $n<=20; $n++){
   if($n%3 === 0 && $n%5 === 0){
     echo "$n FooBar <br/>";
   }
-  else if($n%3 === 0){
+  elseif($n%3 === 0){
      echo "$n Foo <br/>";
   }
-  else if($n%5 === 0){
+  elseif($n%5 === 0){
       echo "$n Bar <br/>";
   }
   else{
