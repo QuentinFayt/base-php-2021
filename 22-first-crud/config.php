4c1eb08ec1805b915433aa2ae917cd935327c4c6
QuentinFayt <quentin.fayt.p@gmail.com>
2021-10-08 10:21:15 +0200

    resuming first crud 08-10-21

--- 22-first-crud/config.php
+++ 22-first-crud/config.php
@@ -3,9 +3,11 @@
 define("MDB_HOST","localhost");
 define("MDB_USER","root");
 define("MDB_PWD","");
-define("MDB_NAME","crud02");
+define("MDB_NAME","crud01");
 define("MDB_PORT","3307");
 define("MDB_CHARSET","utf8");
 
 define("NEWS_HOME_LENGTH",220);
 define("NEWS_BR",true);
+
+define("PUBLIC_PAGES",["home","user","article"]);
--- 22-first-crud/index.php
+++ 22-first-crud/index.php
@@ -6,5 +6,17 @@ $db = @mysqli_connect(MDB_HOST,MDB_USER,MDB_PWD,MDB_NAME,MDB_PORT);
 
 if(!$db){
 
-    die('<h1 style="color:red; display:block; width:90%; margin:0 auto; text-align:center; line-height:90vh">Erreur de connexion: '.mysqli_connect_error()."</h1>");
+    die('<h1 style="color:red; display:block; width:90%; margin:0 auto; text-align:center; line-height:90vh">Erreur de connexion: '.utf8_encode(mysqli_connect_error())."</h1>");
 }
+
+if(isset($_GET['page']) && in_array($_GET["page"],PUBLIC_PAGES)){
+    include "public_page/".$_GET["page"].".php";
+}
+else{
+    header("Location:https://http.cat/404");
+    exit;
+}
+
+
+
+mysqli_close($db);
